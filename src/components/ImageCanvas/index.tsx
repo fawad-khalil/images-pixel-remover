85615d511fa74f574573a01c950b741e3253d3e7
Fawad Khalil <fawad-khalil@hotmail.com>
2023-04-30 23:52:02 +0500

    fix positioning

--- src/components/ImageCanvas/index.tsx
+++ src/components/ImageCanvas/index.tsx
@@ -38,21 +38,21 @@ export const ImageCanvas = ({ image, save, close }: ImageCanvasProps) => {
         close();
     }
 
-    return <div className='flex flex-col bg-opacity-95 bg-gray-600 p-5 pt-52 px-7 z-50 fixed h-full w-full top-0 left-0 justify-center items-center overflow-y-scroll' >
-        <div className={`absolute w-32 h-10 bg-yellow-200 border-[1px] border-black top-1 left-5`}>x={mouseOffset.x}, y={mouseOffset.y}</div>
+    return <div className='flex flex-col bg-opacity-95 bg-gray-600 p-5 pt-6 px-7 z-50 fixed h-full w-full top-0 left-0 items-center overflow-y-scroll' >
+        {/* <div className={`absolute w-32 h-10 bg-yellow-200 border-[1px] border-black top-1 left-5`}>x={mouseOffset.x}, y={mouseOffset.y}</div> */}
 
         <img 
             crossOrigin="anonymous" 
             ref={imageRef} 
             src={imageSrc} 
-            alt="Image in edit" 
+            alt={image.alt_description}
             className="bg-white"
             onClick={onImageClick} 
             onMouseMove={(e) => setMouseOffset({x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY})}/>
         
-        <div className='flex flex-row justify-end mt-5 w-[26%]'>
+        <div className='flex flex-row justify-end mt-5 w-[90%]'>
             <SecondaryButton onClick={close}>Cancel</SecondaryButton>
-            <PrimaryButton classes="px-7" onClick={onClickSave}>Save</PrimaryButton>
+            <PrimaryButton classes="px-7 ml-5" onClick={onClickSave}>Save</PrimaryButton>
         </div>
     </div>
 }
